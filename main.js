9652f244b3eb2cc59d7a8dab929f2c577b4be772
ErikTheCoder <50461511+ErikTheCoder@users.noreply.github.com>
2021-08-06 11:01:28 +0200

    SUS

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 var number = 0;
+var vbucks = 0;
+lastmegalovania = Date.now();
 
 function amogus(){
-    number += 1;
-    document.getElementById("impostor").innerHTML = "YO HAV " + number + "IQ AMOUGUS IMPOSTOR OUPLYS!!!1!!111!!!!"
+    number += 1;   
 }
+
+function sus(){
+    if(number >= 10 * Math.pow(1.01, vbucks)){
+        number -= 10 * Math.pow(1.01, vbucks);
+        vbucks++;
+    }
+}
+
+function sans(megalovania){
+    number += vbucks * (megalovania / 1000);
+}
+
+function undertail(){
+    document.getElementById("impostor").innerHTML = "YO HAV " + number.toFixed(2) + "IQ AMOUGUS IMPOSTOR OUPLYS!!!1!!111!!!!";
+    document.getElementById("papyrus").innerHTML = "PURCHASE VBUCKS NOW FOR" + (10*Math.pow(1.01, vbucks)).toFixed(2) + "AMOGUS IMPOSTOR IQ!!!!!!!!!!!!!!!";
+    document.getElementById("crewmate").innerHTML = "YOU HAEV" + vbucks + "VBUCKS !!!1!!111!!!";
+    document.getElementById("robux").innerHTML = "THYE AR INCRESNG UR ULTRA IMPOSTOR PLAY IQ BY" + vbucks + "PR SECND!!!!!!!!!!"
+}
+
+function communism(){
+    megalovania = Date.now() - lastmegalovania;
+    sans(megalovania);
+    undertail();
+    lastmegalovania = Date.now();
+}
+
+setInterval(communism, 50);
